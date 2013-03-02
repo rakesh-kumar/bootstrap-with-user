64988f7310e6c7564d28ae4655f738f0d698bc46
rakesh <rakeshmaurya262@gmail.com>
2013-03-03 04:31:29 +0530

    bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrap::Application.config.secret_token = '1ca4b9d6ace700145c0dc4918ca23bbe61152af2662a610e7e02119f1fbf06b1cf2c3c690f4ccfe3f92fc6179146f85c7a09b97f816e8087e73c3098134ea3ba'
